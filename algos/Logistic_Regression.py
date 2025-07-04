b3aed0406db1114f44c54b321d3eb39dcff0fdd4
iamprasadraju <workspace.iamprasadraju@gmail.com>
2025-07-04 21:51:18 +0530

    some notes

--- algos/Logistic_Regression.py
+++ algos/Logistic_Regression.py
@@ -1,15 +1,18 @@
 import numpy as np
 import pandas as pd
+from tqdm import tqdm 
 
 """
-Logistic regression is a statistical and machine learning method used to predict the probability of a binary outcome (such as yes/no, success/failure, or 0/1) based on one or more independent variables. 
-Unlike linear regression, which predicts continuous values, logistic regression predicts the probability that a given input belongs to a particular class
+Logistic regression is a statistical and machine learning method used to 
+predict the probability of a binary outcome (such as yes/no, success/failure, or 0/1)
+based on one or more independent variables. Unlike linear regression, which predicts continuous values, 
+logistic regression predicts the probability that a given input belongs to a particular class
 
 """
 
 
 class LogisticRegression:
-    def __init__(self, lr = 0.001, epochs = 1000):
+    def __init__(self, lr = 0.01, epochs = 10000):
         self.lr = lr
         self.epochs = epochs
         self.weights = None
@@ -17,40 +20,33 @@ class LogisticRegression:
 
     def fit(self, X, y):
         num_samples, num_features = X.shape
-        self.weights = np.random.rand(num_features)
-        y_pred = np.dot(X, self.weights) + self.bias
+        self.weights = np.zeros(num_features)
+        for _ in tqdm(range(self.epochs)):
+            y_pred = np.dot(X, self.weights) + self.bias
+            Z = self._sigmoid(y_pred)
+            
+            dw = (1 / num_samples) * np.dot(X.T, (Z - y))
+            db = (1 / num_samples) * np.sum(Z - y)
 
-        print(self._sigmoid(y_pred))
+            self.weights -= self.lr * dw
+            self.bias -= self.lr * db
 
+        return self.weights, self.bias
 
     
     def _sigmoid(self, Z):
+
+        """"
+        Sigmoid (Logistic) Function: Logistic regression uses the sigmoid function 
+        to map any real-valued input into a value between 0 and 1, representing probability
+        """
+
         return 1 / (1 + np.exp(-Z))
-    
 
 
+    def predict(self, X, threshold = 0.5):
+        linear = np.dot(X, self.weights) + self.bias
+        h = self._sigmoid(linear)
+        return (h >= threshold).astype(int)
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-if __name__ == "__main__":
-    data = pd.read_csv("d://Github_Repo's/iamprasadraju/Datasets/breast_cancer.csv")
-    features = data[["radius_mean", "texture_mean"]].to_numpy()
-
-    y_true = data["diagnosis"].map({'B': 0, 'M': 1}).astype(int).to_numpy()
-    
-    model = LogisticRegression()
-    model.fit(features, y_true)
-    
