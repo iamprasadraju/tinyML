8edbe1f45eeb9bdb30127761fd7e6d224d6c3c62
iamprasadraju <workspace.iamprasadraju@gmail.com>
2025-10-04 15:34:45 +0530

    gemm in python

--- ops/gemm.py
+++ ops/gemm.py
@@ -1,40 +1,20 @@
-#!/usr/bin/env  python3
 import time
 import numpy as np
-#np.__config__.show()
 
 
-N = 1024
-
-A = np.random.rand(N, N).astype(np.float32)
-B = np.random.rand(N, N).astype(np.float32)
-
-"""
-FLOP(Floating point operations)
-
-For a Square Matrix
-
-    - Flops per element 
-
-        - Multiplication: n
-        - Additions: n - 1
-
-        Total Flops per element: n+(n-1) = 2n - 1
-
-    Total elements = n^2
-
-so, Total flops = n^2 * (2n - 1) = 2n^3
-
-""" 
-
-flop = 2*N*N*N
-#print(f"{flop / 1e9:.2f} GFLOPS")
-
-for _ in range(10):
-    st = time.monotonic()
-    C = A @ B
-    et = time.monotonic()
-    t = et - st
-    print(f"{flop/t * 1e-12:.2f} TFLOPS")
+N = 1000
 
 
+A = np.random.rand(N, N)
+B = np.random.rand(N, N)
+
+if A.shape[1] == B.shape[0]:
+	C = np.zeros((A.shape[0], B.shape[1]))
+	for i in range(A.shape[0]):
+		for j in range(B.shape[1]):
+			c = 0.0
+			for k in range(A.shape[1]):
+				c += A[i][k] * B[k][j]
+			C[i][j] = c
+else:
+	raise ValueError("Please check the matrices")
