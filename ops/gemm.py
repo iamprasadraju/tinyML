bc5369b97f7e7c200c83b8a70a41ef2685499f6d
iamprasadraju <workspace.iamprasadraju@gmail.com>
2025-10-04 15:36:40 +0530

    updated

--- ops/gemm.py
+++ ops/gemm.py
@@ -2,19 +2,16 @@ import time
 import numpy as np
 
 
-N = 1000
+N = 100
 
 
 A = np.random.rand(N, N)
 B = np.random.rand(N, N)
 
-if A.shape[1] == B.shape[0]:
-	C = np.zeros((A.shape[0], B.shape[1]))
-	for i in range(A.shape[0]):
-		for j in range(B.shape[1]):
-			c = 0.0
-			for k in range(A.shape[1]):
-				c += A[i][k] * B[k][j]
-			C[i][j] = c
-else:
-	raise ValueError("Please check the matrices")
+C = np.zeros((A.shape[0], B.shape[1]))
+for i in range(A.shape[0]):
+	for j in range(B.shape[1]):
+		c = 0.0
+		for k in range(A.shape[1]):
+			c += A[i][k] * B[k][j]
+		C[i][j] = c
