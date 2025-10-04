14045bb548c731e0e6d66f1f67ff9189ceb2110c
iamprasadraju <workspace.iamprasadraju@gmail.com>
2025-10-04 16:32:47 +0530

    perf cal for matmul

---
+++ perf/perf_matmul_np.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env  python3
+import time
+import numpy as np
+#np.__config__.show()
+
+
+N = 1024
+
+A = np.random.rand(N, N).astype(np.float32)
+B = np.random.rand(N, N).astype(np.float32)
+
+"""
+FLOP(Floating point operations)
+
+For a Square Matrix
+
+    - Flops per element 
+
+        - Multiplication: n
+        - Additions: n - 1
+
+        Total Flops per element: n+(n-1) = 2n - 1
+
+    Total elements = n^2
+
+so, Total flops = n^2 * (2n - 1) = 2n^3
+
+""" 
+
+flop = 2*N*N*N
+
+for _ in range(10):
+    st = time.monotonic()
+    C = A @ B
+    et = time.monotonic()
+    t = et - st
+    print(f"{(flop/t) * 1e-9:.2f} GFLOPS")
+
+
