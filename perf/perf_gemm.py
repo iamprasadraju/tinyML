013a5d531bdbf1a808ee10b731fd0418fd94433a
iamprasadraju <workspace.iamprasadraju@gmail.com>
2025-12-05 12:55:59 +0530

    formated

--- perf/perf_gemm.py
+++ perf/perf_gemm.py
@@ -1,14 +1,16 @@
 import time
 import numpy as np
-from debug.helpers import DEBUG, DEBUG_PRINT, FLOPS, N
+from debug.helpers import DEBUG, DEBUG_PRINT, N, timeit
 
+
+@DEBUG_PRINT
 def manymatmul(N):
     A = np.random.rand(N, N).astype(np.float32)
     B = np.random.rand(N, N).astype(np.float32)
     A @ B
     return N * N * N
 
+
 if __name__ == "__main__":
 	while True:
-		GFLOPS, elapsed = FLOPS(manymatmul, N)
-		DEBUG_PRINT(GFLOPS, elapsed)
+		manymatmul(N)
